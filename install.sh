1397fe87ffc143d32b8ff0afcd3e18de2fae855c
usman_said <70-1-4-4-10-70@proton.me>
2026-02-05 17:04:46 +0500

    Add installer script for Kali Linux

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 # Download the JAR from GitHub to /usr/local/bin
-wget -O /usr/local/bin/tictactoe.jar https://github.com/usaihack/TicTacToe-CLI/raw/main/TicTacToe.jar
+wget -O /usr/local/bin/tictactoe.jar https://github.com/usaihack/TicTacToe/raw/main/TicTacToe.jar
 
 # Create a small launcher script
 echo -e '#!/bin/bash\njava -jar /usr/local/bin/tictactoe.jar' > /usr/local/bin/tictactoe
